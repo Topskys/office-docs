4124511a7785e45560e1b60f670fde5b58e01f7a
Topskys <2477350343@qq.com>
2024-12-14 22:05:53 +0800

    feat: 新增工具栏选择字体和切换标题功能

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -2,18 +2,16 @@
 
 import {
   DropdownMenu,
+  DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
+import { Level } from "@tiptap/extension-heading";
 import {
   BoldIcon,
-  CopyIcon,
-  DownloadIcon,
-  HelpCircleIcon,
-  ImportIcon,
-  InfoIcon,
+  ChevronDownIcon,
   ItalicIcon,
   ListTodoIcon,
   LucideIcon,
@@ -21,10 +19,7 @@ import {
   PrinterIcon,
   Redo2Icon,
   RemoveFormattingIcon,
-  Settings2Icon,
-  Share2Icon,
   SpellCheckIcon,
-  Trash2Icon,
   UnderlineIcon,
   Undo2Icon,
 } from "lucide-react";
@@ -81,8 +76,85 @@ const FontFamilyButton = () => {
           <span className="truncate">
             {editor?.getAttributes("textStyle").FontFamily || "Arial"}
           </span>
+          <ChevronDownIcon className="size-4 ml-2 shrink-0" />
         </button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {fonts.map((font) => (
+          <button
+            key={font.value}
+            className={cn(
+              "flex items-center justify-between gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              editor?.getAttributes("textStyle").FontFamily === font.value &&
+                "bg-neutral-200/80"
+            )}
+            style={{ fontFamily: font.value }}
+            onClick={() => {
+              editor?.chain().focus().setFontFamily(font.value).run();
+            }}
+          >
+            <span className="text-sm">{font.name}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+  ];
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", { level })) {
+        return `Heading ${level}`;
+      }
+    }
+    return "Normal text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">{getCurrentHeading()}</span>
+          <ChevronDownIcon className="size-4 ml-2 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            style={{ fontSize }}
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor
+                  ?.chain()
+                  .focus()
+                  .setHeading({ level: value as Level })
+                  .run();
+              }
+            }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              (value === 0 && !editor?.isActive("heading")) ||
+                (editor?.isActive("heading", { level: value }) &&
+                  "bg-neutral-200/80")
+            )}
+          >
+            <span className="text-sm">{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
@@ -168,9 +240,9 @@ const Toolbar = () => {
         <ToolbarButton {...item} key={item.label} />
       ))}
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      {/* TODO: Font family */}
+      <FontFamilyButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      {/* TODO: Heading */}
+      <HeadingLevelButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Font size */}
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
