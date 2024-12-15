f6b81dc1435453a3ea03c9160901470be719467b
Topskys <2477350343@qq.com>
2024-12-15 17:58:30 +0800

    fix: 修复顶部工具栏标尺精确问题

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -69,13 +69,12 @@ export const Ruler = () => {
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
       onMouseUp={handleMouseUp}
-      className="h-6 border-b border-gray-300
+      className="w-[816px] mx-auto h-6 border-b border-gray-300
         flex items-end relative select-none print:hidden"
     >
       <div
         id="ruler-container"
-        className="max-w-[816px] 
-      mx-auto w-full h-full relative"
+        className="w-full h-full relative"
       >
         <Marker
           position={leftMargin}
