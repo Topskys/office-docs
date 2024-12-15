9bbb28cd6ccab02e19e3a790d079b071b3fde17d
Topskys <2477350343@qq.com>
2024-12-15 17:03:31 +0800

    feat: 新增编辑页面标尺及其拖动功能

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import TextAlign from "@tiptap/extension-text-align";
 import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
 import { useEditorStore } from "@/store/use-editor-store";
+import { Ruler } from "./ruler";
 
 const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -121,6 +122,7 @@ const Editor = () => {
 
   return (
     <div className="size-full overflow-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
