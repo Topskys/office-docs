6d80c7645f4da7e6fb62a8694aa37dc68181a266
Topskys <2477350343@qq.com>
2024-12-16 23:00:52 +0800

    feat: 渲染主界面新建文档模板

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,29 @@
-import{ Navbar} from "./navbar";
+"use client";
+import { useQuery } from "convex/react";
+import { Navbar } from "./navbar";
+import { TemplateGallery } from "./template-gallery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
-        <Navbar/>
+        <Navbar />
       </div>
-      <div className="mt-16"></div>
+      <div className="mt-16">
+        <TemplateGallery />
+        {documents?.map((document) => (
+          <div key={document._id}>{JSON.stringify(document)}</div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
